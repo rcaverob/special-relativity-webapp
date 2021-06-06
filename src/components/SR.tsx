d880bf19d5d652be8a6df2de1b257a335df1d539
Rodrigo Cavero Blades <rodrigo.a.cavero.blades.21@dartmouth.edu>
2021-06-06 18:44:13 -0400

    debounced time dilation calculation and added earth png

--- src/components/SR.tsx
+++ src/components/SR.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import Slider, { Range } from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import {
@@ -6,12 +6,11 @@ import {
   validateNumeric,
   formatted,
 } from '../utils/numberUtils';
-// import lottie from 'lottie-web';
-
-import Lottie from 'react-lottie';
-import animationData from '../lotties/rocket-1.json';
+import animationData from '../lotties/rocket-3.json';
 import Select from 'react-select';
-import { handleInputChange } from 'react-select/src/utils';
+import debounce from 'lodash.debounce';
+import Lottie from 'react-lottie';
+import earthImg from '../assets/earth-small.png'; // gives image path
 
 const defaultOptions = {
   loop: true,
@@ -38,9 +37,22 @@ function scale(
   return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
 }
 
+// const debouncedTimeDilation = throttle(
+//   (humanTimeNumeric: number, velocity: number) => {
+//     const ATNum = calculateTimeDilation(humanTimeNumeric, velocity);
+//     return formatted(ATNum);
+//   },
+//   1000,
+//   { leading: true, trailing: false }
+// );
+
 const SR = () => {
+  // Time as measured by observer on Earth
   const [humanTime, setHumanTime] = useState('');
   const humanTimeNumeric = Number(humanTime);
+
+  // Time as measured by observer moving relative to static observer
+  const [alienTime, setAlienTime] = useState('');
 
   // Only Accept numbers and dot in input
   const handleInputChange = (text: string) => {
@@ -48,52 +60,97 @@ const SR = () => {
       setHumanTime(text);
     }
   };
-  // const [alienTime, setAlienTime] = useState(0);
-  const [velocity, setVelocity] = useState(50);
+
+  const [velocity, setVelocity] = useState(0);
 
   // const rocketContainer = useRef<HTMLDivElement>(null);
 
-  const rocketAnimationSpeed = scale(velocity, 0, 100, 0.1, 3);
+  const rocketAnimationSpeed = scale(velocity, 0, 100, 0.1, 6);
 
-  const alienTime = formatted(
-    calculateTimeDilation(humanTimeNumeric, velocity)
+  // const updateTime = debounce((humanTimeNumeric: number, velocity: number) => {
+  //   const ATNum = calculateTimeDilation(humanTimeNumeric, velocity);
+  //   setAlienTime(formatted(ATNum));
+  // }, 1000);
+  // useCallback(
+  //   () => {
+  //     callback
+  //   },
+  //   [],
+  // )
+  const setATDebounced = useCallback(
+    // setAlienTime("loading");
+    debounce((timeNum, v) => {
+      setAlienTime(formatted(calculateTimeDilation(timeNum, v)));
+      // setAlienTime(val);
+    }, 200),
+    []
   );
+
+  useEffect(() => {
+    // const AT = debouncedTimeDilation(humanTimeNumeric, velocity);
+    setAlienTime('Loading');
+    setATDebounced(humanTimeNumeric, velocity);
+    // if (AT) {
+    //   setAlienTime(AT);
+    // }
+    // return () => {
+    //   cleanup
+    // }
+  }, [humanTimeNumeric, setATDebounced, velocity]);
+  // const AT = calculateTimeDilation(humanTimeNumeric, velocity);
+  // console.log(AT);
+
+  // const alienTime = formatted(AT);
+  // const alienTime: string = formatted(
+  //   calculateTimeDilation(humanTimeNumeric, velocity)
+  // );
+
+  console.log('hello');
   return (
     <div className="container">
-      <h3>Human time is: </h3>{' '}
-      <input
-        value={humanTime}
-        onChange={(e) => handleInputChange(e.target.value)}
-      ></input>{' '}
-      <Select
-        className="select"
-        // classNamePrefix="select"
-        defaultValue={timeOptions[0]}
-        // isDisabled={isDisabled}
-        // isLoading={isLoading}
-        // isClearable={isClearable}
-        // isRtl={isRtl}
-        // isSearchable={isSearchable}
-        // name="color"
-        options={timeOptions}
-      />
-      <h3>Alien time is: {alienTime}</h3>
-      <h3>Spaceship Velocity: {velocity}% light speed</h3>
-      <Slider
-        min={0}
-        step={0.01}
-        max={99.99}
-        onChange={(val) => setVelocity(val)}
-      />
-      {/* <div className="anim-container" ref={rocketContainer}></div> */}
-      <div className="no-click">
-        <Lottie
-          options={defaultOptions}
-          height={400}
-          width={400}
-          isPaused={false}
-          speed={rocketAnimationSpeed}
+      <div className="sub-container">
+        <h3>Human time is: </h3>{' '}
+        <input
+          value={humanTime}
+          onChange={(e) => handleInputChange(e.target.value)}
+        ></input>{' '}
+        <Select
+          className="select"
+          // classNamePrefix="select"
+          defaultValue={timeOptions[0]}
+          // isDisabled={isDisabled}
+          // isLoading={isLoading}
+          // isClearable={isClearable}
+          // isRtl={isRtl}
+          // isSearchable={isSearchable}
+          // name="color"
+          options={timeOptions}
         />
+        <img src={earthImg} alt="Earth" className="earth" />
+      </div>
+
+      <div className="sub-container">
+        <h3>Alien time is: {alienTime}</h3>
+        <h3>Spaceship Velocity is: </h3>
+        <h3>{velocity}% light speed</h3>
+        <Slider
+          min={0}
+          step={0.01}
+          max={99.99}
+          onChange={(val) => setVelocity(val)}
+        />
+        {/* <div className="anim-container" ref={rocketContainer}></div> */}
+        <div className="lottie-container">
+          <div className="lottie">
+            <Lottie
+              options={defaultOptions}
+              // height={'100%'}
+              // width={'100%'}
+              isPaused={false}
+              speed={rocketAnimationSpeed}
+            />
+          </div>
+        </div>
       </div>
       {/* <Range /> */}
     </div>
--- src/utils/numberUtils.tsx
+++ src/utils/numberUtils.tsx
@@ -1,16 +1,31 @@
+// import debounce from 'lodash.debounce';
+
 // Returns true only if string consists of numbers and dots only
 export function validateNumeric(s: string) {
   var rgx = /^[0-9]*\.?[0-9]*$/;
   return s.match(rgx);
 }
 
-// Returns the equivalent time duration as measured by an observer moving at the given percentage of light speed
 export function calculateTimeDilation(time: number, lsPercentage: number) {
+  console.log('CALCULATING!!!');
   const lsDecimal = lsPercentage / 100;
   return time * Math.sqrt(1 - lsDecimal ** 2);
 }
 
-export function formatted(value: number) {
+// Returns the equivalent time duration as measured by an observer moving at the given percentage of light speed
+// export const calculateTimeDilation = debounce(
+//   (time: number, lsPercentage: number) => {
+//     console.log('CALCULATING!!!');
+//     const lsDecimal = lsPercentage / 100;
+//     return time * Math.sqrt(1 - lsDecimal ** 2);
+//   },
+//   1000
+// );
+
+export function formatted(value: number | undefined): string {
+  if (value === undefined) {
+    return '';
+  }
   return value.toLocaleString('en-US', {
     minimumFractionDigits: 0,
     maximumFractionDigits: 2,
